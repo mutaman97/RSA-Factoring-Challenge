7783861f9fc5d54f26e59bca719048966c6a07f2
mutaman97 <mutamanelhadi97@gmail.com>
2024-02-11 19:00:50 -0800

    try to fix

--- factors.c
+++ factors.c
@@ -1,46 +1,97 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gmp.h>
 
-int main(int argc, char *argv[])
+/**
+  * factor - factorization handler function
+  * @p: integer pointer
+  * @n: integer
+  *
+  * Return: void
+  */
+void factor(mpz_t *p, mpz_t n)
 {
-	FILE *stream;
+	mpz_t r;
+	
+	mpz_set_ui(*p, 2U);
+
+	if (mpz_divisible_ui_p(n, 2U))
+		return;
+
+	mpz_init(r);
+	mpz_sqrt(r, n);
+	mpz_set_ui(*p, 3U);
+	mpz_sqrt(r, n);
+
+	while (!mpz_divisible_p(n, *p) && mpz_cmp(*p, r) < 0)
+		mpz_add_ui(*p, *p, 2U);
+		
+	if (!mpz_divisible_p(n, *p))
+		mpz_set(*p, n);
+
+	mpz_clear(r);
+}
+
+/**
+  * main - Entry
+  * @ac: argument count
+  * @av: string vector
+  *
+  * Return: integer
+  */
+int main(int ac, char **av)
+{
+	FILE *fp = NULL;
 	char *line = NULL;
-	size_t len = 0;
-	long long flag = 1, div, rest, number, counter;
-	ssize_t nread;
+	size_t line_size = 0;
+	ssize_t line_length = -1;
+	mpz_t n, p, q, r;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+	mpz_init(n);
+	mpz_init(p);
+	mpz_init(q);
+	mpz_init(r);
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Error: usage: factors <file>\n");
 		exit(EXIT_FAILURE);
 	}
 
-	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
-		perror("fopen");
+	fp = fopen(av[1], "r");
+	
+	if (!fp)
+	{
+		fprintf(stderr, "Error: cannot open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	while ((nread = getline(&line, &len, stream)) != -1) {
-		flag = 1, div = 2;
-		number = atoll(line);
-		while (flag) {
-			rest = number % div;
-			if (!rest) {
-				counter = number / div;
-				printf("%lld=%lld*%lld\n", number, counter, div);
-				flag = 0;
-			}
-			div++;
-		}
+	while ((line_length = getline(&line, &line_size, fp)) != -1)
+	{
+		line[line_length - 1] = '\0';
+		
+		mpz_set_str(n, line, 10);
+
+		factor(&p, n);
+		mpz_tdiv_q(q, n, p);
+
+		mpz_out_str(stdout, 10, n);
+		printf("=");
+		mpz_out_str(stdout, 10, q);
+		printf("*");
+		mpz_out_str(stdout, 10, p);
+		printf("\n");
 	}
 
 	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+	fclose(fp);
+
+	mpz_clear(n);
+	mpz_clear(p);
+	mpz_clear(q);
+	mpz_clear(r);
+
+	return (0);
 }
+
